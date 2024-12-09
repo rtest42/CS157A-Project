64cceed3b6d3eec4d3c5753f3aa54738a381fead
rtest42 <royce95129@gmail.com>
2024-12-09 13:26:31 -0800

    Update JDBCUtil.java

--- jdbc-webapp/src/main/java/edu/sjsu/cs157a/util/JDBCUtil.java
+++ jdbc-webapp/src/main/java/edu/sjsu/cs157a/util/JDBCUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class JDBCUtil {
 	private static final String URL = "jdbc:mysql://localhost:3306/jdbc-webapp";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "password";
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
     	Class.forName("com.mysql.cj.jdbc.Driver");
